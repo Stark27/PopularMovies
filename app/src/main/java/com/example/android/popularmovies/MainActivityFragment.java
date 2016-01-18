f982dd87687fe6806b46f600afe58c70f4670de5
Luis Munoz <luismunoz@Luiss-MacBook-Pro.local>
2016-01-17 22:43:51 -0300

    Add Class Movies

--- app/src/main/java/com/example/android/popularmovies/MainActivityFragment.java
+++ app/src/main/java/com/example/android/popularmovies/MainActivityFragment.java
@@ -1,7 +1,7 @@
 package com.example.android.popularmovies;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,6 +17,18 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        Movies[] movies = {
+                new Movies(R.drawable.img1),
+                new Movies(R.drawable.img1),
+                new Movies(R.drawable.img1),
+                new Movies(R.drawable.img1),
+                new Movies(R.drawable.img1),
+                new Movies(R.drawable.img1),
+                new Movies(R.drawable.img1)
+        };
+
+
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
 }
---
+++ app/src/main/java/com/example/android/popularmovies/Movies.java
@@ -0,0 +1,17 @@
+package com.example.android.popularmovies;
+
+/**
+ * Created by luismunoz on 1/17/16.
+ */
+public class Movies {
+    public int image;
+
+    public Movies(){
+        super();
+    }
+
+    public Movies(int image){
+        super();
+        this.image = image;
+    }
+}
